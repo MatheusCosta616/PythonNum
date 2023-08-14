bfa8b96ff28f23db7964f2d5805c6884c4cee1ff
MatheusCosta616 <rm551157@fiap.com.br>
2023-08-14 11:34:39 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+#Matheus José de Lima Costa rm551157
+
+listaAI = []  # Usar uma lista em vez de um conjunto
+listaSom = 0
+
+print("Digite 'real' para adicionar números reais e 'inteiro' para inteiros:")
+resp = input()
+
+while resp != 'real' and resp != 'inteiro':
+    print("Opção inválida. Digite 'real' ou 'inteiro':")
+    resp = input().lower()
+
+if resp == 'real':
+    num_set = set()
+    num_set.add(0.0)
+elif resp == 'inteiro' or 'inteiros':
+    num_set = set()
+    num_set.add(0)
+
+print("Conjunto de números:", num_set)
+
+while listaSom < 10:
+    print("Digite um número:")
+    num = input()
+
+    try:
+        if resp == 'real':
+            num = float(num)
+        elif resp == 'inteiro':
+            num = int(num)
+
+        listaAI.append(num)  # Adicionar o número à lista
+        listaSom = sum(listaAI)
+
+        if listaSom >= 10:
+            break
+    except ValueError:
+        print("Entrada inválida. Digite um número válido.")
+
+maior = max(listaAI)
+
+if resp == 'real' or 'reais':
+    print(f"Você escolheu adicionar números reais.")
+else:
+    print(f"Você escolheu adicionar números inteiros.")
+
+print(f"O maior número é {maior:.1f}" if isinstance(maior, float) else f"O maior número é {int(maior)}")
+
+print("Aqui estão os valores digitados:")
+for valor in listaAI:
+    print(int(valor))
+
+print("Soma dos valores:", listaSom)
